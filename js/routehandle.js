3e73346e1babbd93caa5bdf40b8b4753f882646f
labrusca <labrusca@live.com>
2018-08-27 15:40:41 +0800

    adjust: UI

--- js/routehandle.js
+++ js/routehandle.js
@@ -69,10 +69,10 @@ $().ready(() => {
         success: (feed) => {
             //console.log(feed);
             $('#pagination').twbsPagination({
-                totalPages: Math.ceil(feed.items.length/5),
+                totalPages: Math.ceil(feed.items.length/7),
                 visiblePages: 5,
                 onPageClick: function (event, page) {
-                    getHTMLrender("template/list.html", feed.items.slice((page-1)*5,page*5), "#arl-list", "fadeIn")
+                    getHTMLrender("template/list.html", feed.items.slice((page-1)*7,page*7), "#arl-list", "fadeIn")
                 }
             });
 
@@ -114,7 +114,7 @@ let routes = {
                                                             <div><h1 class="text-center">${filterXSS(title)}</h3></div>
                                                             <hr>
                                                             ${marked(context)}`)
-                                $("#asidemain").append(`<p class="copyleft"><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />一切<span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" rel="dct:type">作品</span>由<a xmlns:cc="http://creativecommons.org/ns#" href="https://labrusca.net/" property="cc:attributionName" rel="cc:attributionURL">labrusca</a>采用<a rel="license" class="text-info" href="http://creativecommons.org/licenses/by-sa/4.0/deed.zh">知识共享署名-相同方式共享 4.0 国际许可协议</a>进行许可。<br />本许可协议授权之外的使用权限可以从 <a class="text-info" xmlns:cc="http://creativecommons.org/ns#" href="https://labrusca.net/#/contact" rel="cc:morePermissions">contact页面</a> 处获得。`)
+                                $("#asidemain").append(`<hr><p class="copyleft"><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />一切<span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" rel="dct:type">作品</span>由<a xmlns:cc="http://creativecommons.org/ns#" href="https://labrusca.net/" property="cc:attributionName" rel="cc:attributionURL">labrusca</a>采用<a rel="license" class="text-info" href="http://creativecommons.org/licenses/by-sa/4.0/deed.zh">知识共享署名-相同方式共享 4.0 国际许可协议</a>进行许可。<br />本许可协议授权之外的使用权限可以从 <a class="text-info" xmlns:cc="http://creativecommons.org/ns#" href="https://labrusca.net/#/contact" rel="cc:morePermissions">contact页面</a> 处获得。`)
                                 $(".copyleft").animateCss('fadeInRight');
                                 $("#pagination").html(`<button id="return" type="button" onClick="location.href='/'" class="btn btn-default btn-lg btn-block">返回</button>`)
                             })
