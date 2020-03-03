0a1f3f884248720909cc2c81e9dc4f05e8ac55d8
Labrusca <labrusca@live.com>
2020-03-03 19:38:53 +0800

    PWA update

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-var cacheName = 'labrusca-net--beta2-test';
+var cacheName = 'labrusca-net--beta3';
 
 var urlsToCache = [
     '/',
     'css/',
-    'img/',
     'js/',
     'blog/index.html',
     'views/navbar.ejs',
@@ -13,7 +12,18 @@ var urlsToCache = [
 
 ];
 
-this.addEventListener('install', function(event) {
+function clearOldCaches() {
+    return caches.keys()
+      .then(keylist => {
+        return Promise.all(
+          keylist
+            .filter(key => key !== cacheName)
+            .map(key => caches.delete(key))
+        );
+      });
+  }
+
+self.addEventListener('install', event => {
     // Perform install steps
     event.waitUntil(
         caches.open(cacheName)
@@ -25,9 +35,9 @@ this.addEventListener('install', function(event) {
 });
 
 // Fetch the contents and reply with cache
-this.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', (event) =>{
     event.respondWith(
-        caches.match(event.request).then(function (response) {
+        caches.match(event.request).then(response => {
             // 来来来，代理可以搞一些代理的事情
 
             // 如果 Service Worker 有自己的返回，就直接返回，减少一次 http 请求
@@ -37,7 +47,7 @@ this.addEventListener('fetch', function (event) {
 
             // 如果 service worker 没有返回，那就得直接请求真实远程服务
             var request = event.request.clone(); // 把原始请求拷过来
-            return fetch(request).then(function (httpRes) {
+            return fetch(request).then(httpRes => {
 
                 // http请求的返回已被抓到，可以处置了。
 
@@ -48,7 +58,7 @@ this.addEventListener('fetch', function (event) {
 
                 // 请求成功的话，将请求缓存起来。
                 var responseClone = httpRes.clone();
-                caches.open(cacheName).then(function (cache) {
+                caches.open(cacheName).then( cache => {
                     cache.put(event.request, responseClone);
                 });
 
@@ -57,3 +67,12 @@ this.addEventListener('fetch', function (event) {
         })
     );
 });
+
+self.addEventListener('activate', event => {
+    console.log('service worker: activate');
+      // delete old caches
+    event.waitUntil(
+      clearOldCaches()
+      .then(() => self.clients.claim())
+      );
+  });
